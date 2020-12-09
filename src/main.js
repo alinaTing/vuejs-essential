89e2b7094195664805471014ef8faeca3e50affb
alina <2964118508@qq.com>
2020-12-09 15:09:15 +0800

    新环境第一次提交

--- src/main.js
+++ src/main.js
@@ -1,8 +1,54 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App'
+import router from './router'
+import './directives'
+import './components'
+import store from './store'
+// 引入插件
+import VueSweetalert2 from './plugins/vue-sweetalert2'
+import Message from './plugins/message'
+import './filters'
+import { mockArticles } from './mock/data'
+import ls from './utils/localStorage'
+// 运行 ./mock/index.js
+import './mock'
+// 引入 axios 的默认值
+import axios from 'axios'
+
+// 将 axios 添加到 Vue.prototype 上，使其在实例内部的所有组件中可用
+Vue.prototype.$axios = axios
+
+
+// 使用插件
+Vue.use(VueSweetalert2)
+Vue.use(Message)
 
 Vue.config.productionTip = false
 
+const AddMockData = (() => {
+  // 是否加入测试数据
+  const isAddMockData = true
+  // 用户数据
+  let userArticles = ls.getItem('articles')
+
+  if (Array.isArray(userArticles)) {
+    userArticles = userArticles.filter(article => parseInt(article.uid) === 1)
+  } else {
+    userArticles = []
+  }
+
+  if (isAddMockData) {
+    // 生成 60 条测试数据
+    store.commit('UPDATE_ARTICLES', [...userArticles, ...mockArticles(60)])
+
+  } else {
+    // 使用用户数据作为所有文章
+    store.commit('UPDATE_ARTICLES', userArticles)
+  }
+})()
+
 new Vue({
+  router,
+  store,
   render: h => h(App),
 }).$mount('#app')
---
+++ src/router/routes.js
@@ -0,0 +1,87 @@
+export default [
+    {
+        path: '/auth/register',
+        name: 'Register',
+        component: () => import('../components/views/auth/Register')
+    },
+    // 首页路由配置
+    {
+        path: '/',
+        name: 'Home',
+        alias: '/topics',
+        component: () => import('../components/views/Home')
+    },
+    // 其他未配置的路由都跳转到首页
+    {
+        path: '*',
+        // 重定向
+        redirect: '/'
+    },
+    {
+        path: '/auth/login',
+        name: 'Login',
+        component: () => import('../components/views/auth/Login')
+    },
+    // EditUsers
+    {
+        path: '/users/1/edit',
+        //name: 'EditUsers',当前路由含有子路由的时候，不需要为当前路由指定name
+        component: () => import('../components/views/users/Edit.vue'),
+        children:[{
+            path:'',
+            name:'EditProfile',
+            component:()=>import('../components/views/users/Profile.vue'),
+            meta:{auth:true}
+        },
+        {
+            path:'/users/1/edit_avatar',
+            name:'EditAvatar',
+            component:()=>import('../components/views/users/Avatar.vue'),
+            meta:{auth:true}
+        },
+        {
+            path:'/users/1/edit_password',
+            name:'EditPassword',
+            component:()=>import('../components/views/users/Password.vue'),
+            meta:{auth:true}
+        }
+            ]
+    },
+    // Create
+    {
+        path: '/articles/create',
+        name:'Create',
+        component: () => import('../components/views/articles/Create'),
+        meta: { auth: true }
+    },
+    // Content
+    {
+        path: '/articles/:articleId/content',
+        name: 'Content',
+        component: () => import('../components/views/articles/Content.vue')
+    },
+    // Edit
+    {
+        path: '/articles/:articleId/edit',
+        name: 'Edit',
+        component: () => import('../components/views/articles/Create'),
+        meta: { auth: true }
+    },
+    // Column
+    {
+        path: '/:user',
+        //name: 'Column',
+        component: () => import('../components/views/articles/Column'),
+        children: [{
+            path: '',
+            name: 'Column',
+            component:()=>import('../components/views/articles/List.vue')
+        },
+        {
+            path: '/articles/:articleId/content',
+            name: 'Content',
+            component: () => import('../components/views/articles/Content.vue')
+        }]
+    },
+
+]
